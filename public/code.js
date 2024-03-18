0117f62a06f70d81b46b3abfa411a584aa9e7812
Meduza3 <jellyfish200325@gmail.com>
2024-03-18 01:00:15 +0100

    Working!

--- public/code.js
+++ public/code.js
@@ -30,25 +30,208 @@ function pollServerForUpdates() {
         url: "ustawa",
         type: "GET",
         success: function(data) {
-            //$(".axis_block").css('background-color', 'white');
+            $(".axis_block").css('background-color', 'white');
             // Assuming data.axisA contains a value that completes the ID of the element you're targeting
             console.log(data.axisA, data.axisB, data.axisC, data.axisD)
-            $(`#AxisA${data.axisA}`).css('background-color', 'red');
-            $(`#AxisA${data.axisA - 1}`).css('background-color', 'red');
-            $(`#AxisA${data.axisA + 1}`).css('background-color', 'red');
 
-            console.log(`axisA${data.axisA}`)
-            $(`#AxisB${data.axisB}`).css('background-color', 'red');
-            $(`#AxisB${data.axisB - 1}`).css('background-color', 'red');
-            $(`#AxisB${data.axisB + 1}`).css('background-color', 'red');
+            if(data.axisA != 0){
+                switch (data.axisA) {
+                    case -4:
+                        $("#axisA-4").css('background-color', 'red');
+                        $("#axisA-3").css('background-color', 'red');
+                        break
+                    
+                    case -3:
+                        $("#axisA-4").css('background-color', 'red');
+                        $("#axisA-3").css('background-color', 'red');
+                        $("#axisA-2").css('background-color', 'red');
+                        break
 
-            $(`#AxisC${data.axisC}`).css('background-color', 'red');
-            $(`#AxisC${data.axisC - 1}`).css('background-color', 'red');
-            $(`#AxisC${data.axisC + 1}`).css('background-color', 'red');
+                    case -2:
+                        $("#axisA-3").css('background-color', 'red');
+                        $("#axisA-2").css('background-color', 'red');
+                        $("#axisA-1").css('background-color', 'red');
+                        break
+                    
+                    case -1:
+                        $("#axisA-2").css('background-color', 'red');
+                        $("#axisA-1").css('background-color', 'red');
+                        $("#axisA01").css('background-color', 'red');
+                        break
 
-            $(`#AxisD${data.axisD}`).css('background-color', 'red');
-            $(`#AxisD${data.axisD - 1}`).css('background-color', 'red');
-            $(`#AxisD${data.axisD + 1}`).css('background-color', 'red');
+                    case 1:
+                        $("#axisA-1").css('background-color', 'red');
+                        $("#axisA01").css('background-color', 'red');
+                        $("#axisA02").css('background-color', 'red');
+                        break
+
+                    case 2:
+                        $("#axisA01").css('background-color', 'red');
+                        $("#axisA02").css('background-color', 'red');
+                        $("#axisA03").css('background-color', 'red');
+                        break
+
+                    case 3:
+                        $("#axisA02").css('background-color', 'red');
+                        $("#axisA03").css('background-color', 'red');
+                        $("#axisA04").css('background-color', 'red');
+                        break
+
+                    case 4:
+                        $("#axisA03").css('background-color', 'red');
+                        $("#axisA04").css('background-color', 'red');
+                        break
+                }
+            }
+            if(data.axisB != 0){
+                switch (data.axisB) {
+                    case -4:
+                        $("#axisB-4").css('background-color', 'red');
+                        $("#axisB-3").css('background-color', 'red');
+                        break
+                    
+                    case -3:
+                        $("#axisB-4").css('background-color', 'red');
+                        $("#axisB-3").css('background-color', 'red');
+                        $("#axisB-2").css('background-color', 'red');
+                        break
+
+                    case -2:
+                        $("#axisB-3").css('background-color', 'red');
+                        $("#axisB-2").css('background-color', 'red');
+                        $("#axisB-1").css('background-color', 'red');
+                        break
+                    
+                    case -1:
+                        $("#axisB-2").css('background-color', 'red');
+                        $("#axisB-1").css('background-color', 'red');
+                        $("#axisB01").css('background-color', 'red');
+                        break
+
+                    case 1:
+                        $("#axisB-1").css('background-color', 'red');
+                        $("#axisB01").css('background-color', 'red');
+                        $("#axisB02").css('background-color', 'red');
+                        break
+
+                    case 2:
+                        $("#axisB01").css('background-color', 'red');
+                        $("#axisB02").css('background-color', 'red');
+                        $("#axisB03").css('background-color', 'red');
+                        break
+
+                    case 3:
+                        $("#axisB02").css('background-color', 'red');
+                        $("#axisB03").css('background-color', 'red');
+                        $("#axisB04").css('background-color', 'red');
+                        break
+
+                    case 4:
+                        $("#axisB03").css('background-color', 'red');
+                        $("#axisB04").css('background-color', 'red');
+                        break
+                }
+            }
+            if(data.axisC != 0){
+                switch (data.axisC) {
+                    case -4:
+                        $("#axisC-4").css('background-color', 'red');
+                        $("#axisC-3").css('background-color', 'red');
+                        break
+                    
+                    case -3:
+                        $("#axisC-4").css('background-color', 'red');
+                        $("#axisC-3").css('background-color', 'red');
+                        $("#axisC-2").css('background-color', 'red');
+                        break
+
+                    case -2:
+                        $("#axisC-3").css('background-color', 'red');
+                        $("#axisC-2").css('background-color', 'red');
+                        $("#axisC-1").css('background-color', 'red');
+                        break
+                    
+                    case -1:
+                        $("#axisC-2").css('background-color', 'red');
+                        $("#axisC-1").css('background-color', 'red');
+                        $("#axisC01").css('background-color', 'red');
+                        break
+
+                    case 1:
+                        $("#axisC-1").css('background-color', 'red');
+                        $("#axisC01").css('background-color', 'red');
+                        $("#axisC02").css('background-color', 'red');
+                        break
+
+                    case 2:
+                        $("#axisC01").css('background-color', 'red');
+                        $("#axisC02").css('background-color', 'red');
+                        $("#axisC03").css('background-color', 'red');
+                        break
+
+                    case 3:
+                        $("#axisC02").css('background-color', 'red');
+                        $("#axisC03").css('background-color', 'red');
+                        $("#axisC04").css('background-color', 'red');
+                        break
+
+                    case 4:
+                        $("#axisC03").css('background-color', 'red');
+                        $("#axisC04").css('background-color', 'red');
+                        break
+                }
+            }
+            if(data.axisD != 0){
+                switch (data.axisD) {
+                    case -4:
+                        $("#axisD-4").css('background-color', 'red');
+                        $("#axisD-3").css('background-color', 'red');
+                        break
+                    
+                    case -3:
+                        $("#axisD-4").css('background-color', 'red');
+                        $("#axisD-3").css('background-color', 'red');
+                        $("#axisD-2").css('background-color', 'red');
+                        break
+
+                    case -2:
+                        $("#axisD-3").css('background-color', 'red');
+                        $("#axisD-2").css('background-color', 'red');
+                        $("#axisD-1").css('background-color', 'red');
+                        break
+                    
+                    case -1:
+                        $("#axisD-2").css('background-color', 'red');
+                        $("#axisD-1").css('background-color', 'red');
+                        $("#axisD01").css('background-color', 'red');
+                        break
+
+                    case 1:
+                        $("#axisD-1").css('background-color', 'red');
+                        $("#axisD01").css('background-color', 'red');
+                        $("#axisD02").css('background-color', 'red');
+                        break
+
+                    case 2:
+                        $("#axisD01").css('background-color', 'red');
+                        $("#axisD02").css('background-color', 'red');
+                        $("#axisD03").css('background-color', 'red');
+                        break
+
+                    case 3:
+                        $("#axisD02").css('background-color', 'red');
+                        $("#axisD03").css('background-color', 'red');
+                        $("#axisD04").css('background-color', 'red');
+                        break
+
+                    case 4:
+                        $("#axisD03").css('background-color', 'red');
+                        $("#axisD04").css('background-color', 'red');
+                        break
+                }
+            }
+            
+
         },
         error: function(xhr, status, error) {
             console.error("Error: " + error);
@@ -94,7 +277,7 @@ function joinGame() {
 
 // Clickables, Event Listeners, Interactables
 $(document).ready(function() {
-    setInterval(pollServerForUpdates, 3000);
+    setInterval(pollServerForUpdates, 500);
     $.getJSON("join", (data, status) => {
         console.log(data)
     })
